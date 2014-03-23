8894a40959363fe2b430040dc2cc955682369f0e
itsbrnrd <mathieu@brnrd.fr>
2014-03-23 17:24:13 +0100

    Added a gulp task to copy fonts and changed 'build' task

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,14 @@ var csslint = require('gulp-csslint');
 
 var paths = {
     dist: 'dist',
-    html: ['samples/*.html'],
+    html: 'samples/*.html',
     sass: ['dessert.scss', '*/*.scss'],
-    css: ['dist/*.css']
+    css: 'dist/*.css',
+    fonts: 'fonts/**/*.*'
 };
 
-gulp.task('build', function() {
+// Compile SASS (compressed)
+gulp.task('sass:dist', function() {
     return gulp.src('dessert.scss')
         .pipe(sass({ style: 'compressed' }))
         .pipe(autoprefixer('last 2 version', 'ios 6', 'android 4'))
@@ -23,13 +25,33 @@ gulp.task('build', function() {
         .pipe(gulp.dest(paths.dist));
 });
 
+// Compile SASS (expanded)
+gulp.task('sass', function() {
+    return gulp.src('dessert.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'ios 6', 'android 4'))
+        .pipe(csslint('csslintrc.json'))
+        .pipe(csslint.reporter())
+        .pipe(gulp.dest(paths.dist));
+});
+
+// Copy all fonts
+gulp.task('fonts', function() {
+ return gulp.src(paths.fonts, { base: './' })
+    .pipe(gulp.dest(paths.dist));
+});
+
+// Watches change in SASS files
 gulp.task('watch', function() {
     gulp.watch([paths.sass], ['build']);
 });
 
+// Clean dist directory
 gulp.task('clean', function() {
     return gulp.src(paths.dist, {read: false})
         .pipe(clean());
 });
 
+gulp.task('build:dist', ['fonts', 'sass:dist']);
+gulp.task('build', ['fonts', 'sass']);
 gulp.task('default', ['build']);
